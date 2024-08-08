a18425d74b2ead3368eed456c382a100a6fa2e12
michaelangelovalente <miky10.mv@gmail.com>
2024-08-08 05:45:35 +0200

    Fixed security config for response body in case of 401

--- src/main/java/com/beije/account_service_be/system_global/config/security/SecurityConfig.java
+++ src/main/java/com/beije/account_service_be/system_global/config/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.beije.account_service_be.system_global.config.security;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,8 +8,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
